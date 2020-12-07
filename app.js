af03969d8f80e1d669d953b031132716d406bdd8
codelifee <suminjung09@gmail.com>
2020-12-07 18:57:14 +0900

    resolved navigation animation but still need to resolve the height of navgation-bar and transition

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const burger = document.querySelector(".burger");
-const nav = document.querySelector(".nav-links");
+const nav = document.querySelector("#nav-links");
+const navLinks = document.querySelectorAll(".nav-links li");
+const navbar = document.querySelector(".nav");
 
 
 burger.addEventListener('click', () => {
-    console.log("hello");
 
-    console.log(nav);
-    console.log(nav.style);
+    // console.log(navbar.style.z-index);
 
-    nav.style.transfrom = 'translateX(0px)';
+    if(nav.style.transform == "translateX(100%)"){
+        nav.style.setProperty("transform", "translateX(0%)");
+        navbar.style.zIndex = "100";
+        navbar.style.height = "130rem";
+    } else {
+        nav.style.setProperty("transform", "translateX(100%)");
+        navbar.style.zIndex = "0";
+        navbar.style.height = "500px";
+    }
 
+
+    // nav.classList.toggle(".nav-active");
 
 });
+
+
+// nav -> z-index: 100;
+// height: 110rem;
