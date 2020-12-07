05c050de32ea3ae4075d3fc5fbf6a0ed8f0a9e54
codelifee <suminjung09@gmail.com>
2020-12-07 21:35:00 +0900

    completed responsiveness except for the height of the grid problem

--- app.js
+++ app.js
@@ -6,23 +6,28 @@ const navbar = document.querySelector(".nav");
 
 burger.addEventListener('click', () => {
 
-    // console.log(navbar.style.z-index);
+    console.log(navbar.offsetWidth);    
 
     if(nav.style.transform == "translateX(100%)"){
-        nav.style.setProperty("transform", "translateX(0%)");
+        nav.style.transform = "translateX(0%)"
+        // nav.style.setProperty("transform", "translateX(0%)");
         navbar.style.zIndex = "100";
         navbar.style.height = "130rem";
     } else {
-        nav.style.setProperty("transform", "translateX(100%)");
+        nav.style.transform = "translateX(100%)"
+        // nav.style.setProperty("transform", "translateX(100%)");
         navbar.style.zIndex = "0";
-        navbar.style.height = "500px";
     }
-
-
-    // nav.classList.toggle(".nav-active");
 
 });
 
+function myfunction(x) {
+    console.log("working");
+    if (x.matches) {
+        nav.style.setProperty("transform", "translateX(0%)");
+    }
+}
 
-// nav -> z-index: 100;
-// height: 110rem;
+let x = window.matchMedia("(min-width: 950px)")
+myfunction(x);
+x.addListener(myfunction);
